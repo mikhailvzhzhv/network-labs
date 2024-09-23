cacd9143e29a5b9621734cdd8f99d732f8ee7d82
mikhail <mihaelvozhzhov@gmail.com>
2024-09-24 00:44:10 +0700

    upd copy finder

--- lab1/copy_finder.py
+++ lab1/copy_finder.py
@@ -17,60 +17,60 @@ def stars_boarded(f):
 
 class CopyFinder:
     def __init__(self, MC_GROUP) -> None:
-        self.__MC_GROUP: str = MC_GROUP
-        self.__PORT: int = 1234
-        self.__ADDRESS: tuple = (self.__MC_GROUP, self.__PORT)
-        self.__copies: dict = dict()
-        self.__diconnected_copies: list = list()
-        self.__ttl: int = 32
-        self.__send_timer: Timer = Timer(500)
-        self.__family = socket.AF_INET6 if ":" in self.__MC_GROUP else socket.AF_INET
-        self.__sock_sender: sock = sock(
-            self.__family, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+        self.MC_GROUP: str = MC_GROUP
+        self.PORT: int = 1234
+        self.ADDRESS: tuple = (self.MC_GROUP, self.PORT)
+        self.copies: dict = dict()
+        self.diconnected_copies: list = list()
+        self.ttl: int = 32
+        self.send_timer: Timer = Timer(500)
+        self.family = socket.AF_INET6 if ":" in self.MC_GROUP else socket.AF_INET
+        self.sock_sender: sock = sock(
+            self.family, socket.SOCK_DGRAM, socket.IPPROTO_UDP
         )
-        self.__sock_receiver: sock = sock(
-            self.__family, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+        self.sock_receiver: sock = sock(
+            self.family, socket.SOCK_DGRAM, socket.IPPROTO_UDP
         )
 
-        match self.__family:
+        match self.family:
             case socket.AF_INET:
-                self.__sock_sender.setsockopt(
-                    socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, self.__ttl
+                self.sock_sender.setsockopt(
+                    socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, self.ttl
                 )
-                self.__sock_receiver.setsockopt(
+                self.sock_receiver.setsockopt(
                     socket.SOL_SOCKET, socket.SO_REUSEADDR, 1
                 )
-                self.__sock_receiver.bind(("0.0.0.0", self.__PORT))
+                self.sock_receiver.bind(("0.0.0.0", self.PORT))
 
                 mreq = struct.pack(
-                    "4sl", socket.inet_aton(self.__MC_GROUP), socket.INADDR_ANY
+                    "4sl", socket.inet_aton(self.MC_GROUP), socket.INADDR_ANY
                 )
-                self.__sock_receiver.setsockopt(
+                self.sock_receiver.setsockopt(
                     socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq
                 )
             case socket.AF_INET6:
-                self.__sock_sender.setsockopt(
-                    socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, self.__ttl
+                self.sock_sender.setsockopt(
+                    socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, self.ttl
                 )
-                self.__sock_receiver.setsockopt(
+                self.sock_receiver.setsockopt(
                     socket.SOL_SOCKET, socket.SO_REUSEADDR, 1
                 )
-                self.__sock_receiver.bind(("::", self.__PORT))
+                self.sock_receiver.bind(("::", self.PORT))
 
                 mreq = struct.pack(
                     "16s15s",
-                    socket.inet_pton(socket.AF_INET6, self.__MC_GROUP),
+                    socket.inet_pton(socket.AF_INET6, self.MC_GROUP),
                     b"\0" * 15 + b"\001",
                 )
-                self.__sock_receiver.setsockopt(
+                self.sock_receiver.setsockopt(
                     socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq
                 )
 
-        self.__sock_receiver.settimeout(0.5)
+        self.sock_receiver.settimeout(0.5)
 
     @stars_boarded
-    def __print_addrs(self) -> None:
-        for address in self.__copies.keys():
+    def print_addrs(self) -> None:
+        for address in self.copies.keys():
             print(address)
 
     def find_copy(self) -> None:
@@ -79,36 +79,36 @@ class CopyFinder:
         while True:
             try:
                 while True:
-                    _, addr_from = self.__sock_receiver.recvfrom(0)
-                    if not self.__copies.get(addr_from):
-                        self.__copies[addr_from] = Timer(4000)
+                    _, addr_from = self.sock_receiver.recvfrom(0)
+                    if not self.copies.get(addr_from):
+                        self.copies[addr_from] = Timer(4000)
                         print(f"Copy from {addr_from} connected")
-                        self.__print_addrs()
-                    life_timer: Timer = self.__copies.get(addr_from)
+                        self.print_addrs()
+                    life_timer: Timer = self.copies.get(addr_from)
                     life_timer.reset()
             except TimeoutError:
                 pass
 
-            if self.__send_timer.expired():
-                self.__sock_sender.sendto(bytes([]), self.__ADDRESS)
-                self.__send_timer.reset()
+            if self.send_timer.expired():
+                self.sock_sender.sendto(bytes([]), self.ADDRESS)
+                self.send_timer.reset()
 
-            for address, life_timer in self.__copies.items():
+            for address, life_timer in self.copies.items():
                 if life_timer.expired():
-                    self.__diconnected_copies.append(address)
+                    self.diconnected_copies.append(address)
 
-            if len(self.__diconnected_copies) != 0:
-                for address in self.__diconnected_copies:
+            if len(self.diconnected_copies) != 0:
+                for address in self.diconnected_copies:
                     print(f"Copy from {address} disconnected")
-                    self.__copies.pop(address)
+                    self.copies.pop(address)
 
-                self.__print_addrs()
-                self.__diconnected_copies.clear()
+                self.print_addrs()
+                self.diconnected_copies.clear()
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Enter multicast group address")
+        print("Wanted <multicast group address>")
         exit()
     multicast_group: str = str(sys.argv[1])
     copy_finder: CopyFinder = CopyFinder(multicast_group)
--- lab1/timer.py
+++ lab1/timer.py
@@ -8,14 +8,14 @@ class MillisGetter:
 
 class Timer(MillisGetter):
     def __init__(self, millis: int) -> None:
-        self.__ms: int = millis
-        self.__start_time: int = self.get_cur_ms()
+        self.ms: int = millis
+        self.start_time: int = self.get_cur_ms()
 
     def get_timeout(self) -> int:
-        return self.__ms
+        return self.ms
 
     def expired(self) -> bool:
-        return self.get_cur_ms() > self.__start_time + self.__ms
+        return self.get_cur_ms() > self.start_time + self.ms
 
     def reset(self) -> None:
-        self.__start_time = self.get_cur_ms()
+        self.start_time = self.get_cur_ms()
